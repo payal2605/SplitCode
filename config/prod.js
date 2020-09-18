a3faea9ababf805716699db2521ef96ec3bae6c8
payal2605 <42538269+payal2605@users.noreply.github.com>
2020-09-19 00:36:56 +0530

    Update prod.js

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,5 @@ module.exports = {
 	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
 	mongoURI: process.env.MONGO_URI,
 	cookieKey: process.env.COOKIE_KEY,
-	URL: <HEROKU-APP>
+	URL: <HEROKU-APP-LINK>
 }
