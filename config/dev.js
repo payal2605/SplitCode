8514db4a917833a21f0f26e9d6670d22f5da6374
payal2605 <42538269+payal2605@users.noreply.github.com>
2020-09-19 00:31:21 +0530

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,7 @@
 module.exports = {
-    googleClientId: '119273365006-nita54fjtb750r6otitn8nf9v98lbngg.apps.googleusercontent.com',
-	googleClientSecret: 'KdQDUs9mQ2_uSkCGqQ_a_8mD',
-	mongoURI: 'mongodb+srv://payal:mongodb1234@cluster0.5iqbo.mongodb.net/db?retryWrites=true&w=majority',
-	cookieKey: 'skjddddddddddksdjkodksdkodjojdsojdsjdskjkjdlejeojfekheoijeojddkdwkdkkekeewkle',
-	URL: 'http://localhost:8000'
+    googleClientId: <GOOGLE-CLIENT-ID>,
+    googleClientSecret: <GOOGLE-CLIENT-SECRET>,
+    mongoURI: <DATABASE-NAME>,
+    cookieKey: <RANDOM-VALUE>,
+    URL: 'http://localhost:8000'
 }
