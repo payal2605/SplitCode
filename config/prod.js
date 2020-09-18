4263752df6b943b9d99bf77314c1c14fdf2749d8
payal2605 <42538269+payal2605@users.noreply.github.com>
2020-09-19 00:32:56 +0530

    Added prod.js

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+    googleClientId: process.env.GOOGLE_CLIENT_ID,
+	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+	//mongoURI: process.env.MONGO_URI,
+	mongoURI: 'mongodb+srv://payal:mongodb1234@cluster0.5iqbo.mongodb.net/db?retryWrites=true&w=majority',
+	cookieKey: process.env.COOKIE_KEY,
+	URL: 'https://splitcode.herokuapp.com'
+}
